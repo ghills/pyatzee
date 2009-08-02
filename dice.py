6994e3486ffa3c03e1346ecd6b72045f90d232fc
ghills <ghills@gmail.com>
2009-08-02 15:40:16 -0700

    added doc header and changed roll function to use variable side number

--- dice.py
+++ dice.py
@@ -1,5 +1,8 @@
 import random
-
+"""
+Dice class, represents a die in a game.
+Pass it an integer for the number of sides it should have.
+"""
 class dice(object):
 	held = False
 
@@ -9,7 +12,7 @@ class dice(object):
 		self.value = sides
 
 	def roll(self):
-		self.value = random.randint(1,6)
+		self.value = random.randint(1,self.sides)
 
 	def __str__(self):
 		return str(self.value)
