6ce50650d62dd8245a59d42826c88387510b7f3b
ghills <ghills@gmail.com>
2009-08-02 16:29:01 -0700

    initial workings of score checking implemented, but does not yet have the entire set of possible scoring possibilities

--- sandbox.py
+++ sandbox.py
@@ -2,6 +2,7 @@
 
 from dice import dice
 from hand import hand
+from scores import scoreengine
 
 """
 x = dice(6)
@@ -14,4 +15,7 @@ h = hand()
 hand.dice[0].held = True
 print [d.value for d in hand.dice]
 print [d.held for d in hand.dice]
+
+se = scoreengine()
+print ["%s: %d" % (s.title, s.score) for s in se.get_possible(h)]
 
--- scores.py
+++ scores.py
@@ -1,19 +1,50 @@
 class scoreengine(object):
-	score_types = []
-
-	def __init__(self):
-		self.populate_scores()
 
 	def get_possible(self, hand):
-		return score_types
+		possible_scores = []
+		#Ones
+		possible_scores.append(score("Ones",self.check_num(1,hand)))
+		#Twos
+		possible_scores.append(score("Twos",self.check_num(2,hand)))
+		#Threes
+		possible_scores.append(score("Threes",self.check_num(3,hand)))
+		#Fours
+		possible_scores.append(score("Fours",self.check_num(4,hand)))
+		#Fives
+		possible_scores.append(score("Fives",self.check_num(5,hand)))
+		#Sixes
+		possible_scores.append(score("Sixes",self.check_num(6,hand)))
+		#Three of a kind
+		possible_scores.append(score("Three of a kind",self.check_of_a_kind(3,hand)))
+		#Four of a kind
+		possible_scores.append(score("Three of a kind",self.check_of_a_kind(4,hand)))
+		#Full House
+		#Small Straight
+		#Large Straight
+		#Chance
+		possible_scores.append(score("Chance",sum([x.value for x in hand.dice])))
+		#Yatzee
+		return filter(lambda x: x.score != 0,possible_scores)
 	
-	def populate_scores(self):
-		#populate the possible score types
-		score_types.append(score("ones",1,1))
+	def check_num(self,n,hand):
+		counter = 0
+		for d in hand.dice:
+			if d.value == n: counter += n
+		return counter
+	
+	def check_of_a_kind(self,n,hand):
+		vals = []
+		for d in hand.dice:
+			vals.append(d.value)
+		for i in range(1,6):
+			if vals.count(i) >= n:
+				return sum(vals)
+		return 0
+				
+		
 	
 class score(object):
 	
-	def __init__(self, title, score, check_function):
+	def __init__(self, title, score):
 		self.title = title
 		self.score = score
-		self.check_function = check_function
